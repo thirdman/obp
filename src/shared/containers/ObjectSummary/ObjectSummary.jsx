02c5e8769deb542004bc1a7602e9fa8883326b35
Gareth Bedford <g@thirdman.co.nz>
2016-09-20 10:14:12 +1200

    fix(contentItem): padding bug

--- src/shared/containers/ObjectSummary/ObjectSummary.jsx
+++ src/shared/containers/ObjectSummary/ObjectSummary.jsx
@@ -26,35 +26,47 @@ export default class ObjectSummary extends Component {
 						<ObjectInfo key={'layoutHero'} title="This is a test title" />
 						<Row isFlex classNameProps={['hasPadding']}>
 							<Column occupy={4} of={12}>
-								<ContentItem title="Total rent" hasDivider>
+								<ContentItem
+									title="Total rent"
+									hasDivider
+									classNameProps={['noMargin', 'paddingLeft']}
+									>
 									<Info content="$1245" />
 								</ContentItem>
-								<ContentItem title="Monthly Rent" hasDivider>
+								<ContentItem
+									title="Monthly Rent"
+									hasDivider
+									classNameProps={['noMargin', 'paddingLeft']}
+									>
 									<Info content="$103" />
 								</ContentItem>
-								<ContentItem title="Payment Date" hasDivider>
+								<ContentItem
+									title="Payment Date"
+									hasDivider
+									classNameProps={['noMargin', 'paddingLeft']}
+									>
 									<Info content="1st monday of each month" />
 								</ContentItem>
 							</Column>
 							<Column occupy={4} of={12}>
-								<ContentItem title="next event date" hasDivider>
+								<ContentItem title="next event date" hasDivider classNameProps={['noMargin']}>
 									<Info content="12 Jan 2014" />
 								</ContentItem>
-								<ContentItem title="Next event" hasDivider>
+								<ContentItem title="Next event" hasDivider classNameProps={['noMargin']}>
 									<Info content="Rent Review" />
 								</ContentItem>
-								<ContentItem title="Event status" hasDivider>
+								<ContentItem title="Event status" hasDivider classNameProps={['noMargin']}>
 									<Info content="Upcoming" />
 								</ContentItem>
 							</Column>
 							<Column occupy={4} of={12} isLast>
-								<ContentItem title="Matter references" hasDivider>
+								<ContentItem title="Matter references" hasDivider classNameProps={['noMargin']}>
 									<Info content="Khanh, Is, Cool" />
 								</ContentItem>
-								<ContentItem title="Agreement Id" hasDivider>
+								<ContentItem title="Agreement Id" hasDivider classNameProps={['noMargin']}>
 									<Info content="12345" />
 								</ContentItem>
-								<ContentItem title="Organisation" hasDivider hasBackground>
+								<ContentItem title="Organisation" hasDivider classNameProps={['noMargin']}>
 									<Info content="Dee bee company" />
 								</ContentItem>
 							</Column>
