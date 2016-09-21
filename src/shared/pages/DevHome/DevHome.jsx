fc6ff59b54ff032a8ad33d29cd69d148923d123c
Gareth Bedford <g@thirdman.co.nz>
2016-09-22 09:34:51 +1200

    fix: sections.

--- src/shared/pages/DevHome/DevHome.jsx
+++ src/shared/pages/DevHome/DevHome.jsx
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 import { Overview } from 'layouts';
-import { Column, SubNavWrap, Row } from 'components';
+import { Column, Row, SubNavWrap, Section } from 'components';
 import { Header } from 'containers';
 
 // const globalStyles = require('../App/App.scss');
@@ -22,40 +22,45 @@ export default class DevIcons extends Component {
 					]}
 				/>
 				<div key={'layoutMain'} className={styles.DevHome}>
-					<h3>Links</h3>
-					<Row>
-						<Column occupy="4" of="12">
-							<h4>Documentation</h4>
-							<a link="sdf">Front end</a>
-							<a link="sdf">`Api (whiteboard)`</a>
-							<a link="sdf">Icons</a>
-							<a link="github">Github</a>
-						</Column>
-						<Column occupy="4" of="12">
-							<h4>Servers</h4>
-							<a link="https://dev-api.nomosone.com">dev-api</a>
-							<a link="https://test-api.nomosone.com">test-api</a>
-							<a link="https://api.nomosone.com">prod: api</a>
-							<a link="https://test-app.nomosone.com">test-app</a>
-							<a link="https://app.nomosone.com">prod: app</a>
-						</Column>
-						<Column occupy="4" of="12">
-							<h4>Integrations</h4>
-							<a link="https://xero.com">Xero</a>
-						</Column>
-					</Row>
-					<Row>
-						<Column occupy="12">
-							<h3>Status</h3>
-						</Column>
-						<div>status will be here</div>
-					</Row>
-					<Row>
-						<Column occupy="12">
-							<h3>Statistics</h3>
-						</Column>
-						<div>Active users, endpoints accessed, pageviews, events progressed, popular pages</div>
-					</Row>
+					<Section title="Links" type="section">
+						<Row>
+							<Column occupy="4" of="12">
+								<h4>Documentation</h4>
+								<a link="sdf">Front end</a>
+								<a link="sdf">`Api (whiteboard)`</a>
+								<a link="sdf">Icons</a>
+								<a link="github">Github</a>
+							</Column>
+							<Column occupy="4" of="12">
+								<h4>Servers</h4>
+								<a link="https://dev-api.nomosone.com">dev-api</a>
+								<a link="https://test-api.nomosone.com">test-api</a>
+								<a link="https://api.nomosone.com">prod: api</a>
+								<a link="https://test-app.nomosone.com">test-app</a>
+								<a link="https://app.nomosone.com">prod: app</a>
+							</Column>
+							<Column occupy="4" of="12">
+								<h4>Integrations</h4>
+								<a link="https://xero.com">Xero</a>
+							</Column>
+						</Row>
+					</Section>
+					<Section title="Status" hasDivider>
+						<Row>
+							<Column occupy="12">
+								<h3>Status</h3>
+							</Column>
+							<div>status will be here</div>
+						</Row>
+					</Section>
+					<Section title="Statistics" hasDivider>
+						<Row>
+							<div>
+								Active users, endpoints accessed,
+								pageviews, events progressed, popular pages
+							</div>
+						</Row>
+					</Section>
 				</div>
 				<div key={'layoutSecondary'}>
 					Reports will be here.
