636c70acf730857bf1d349569b4f4e3747f971cd
Gareth Bedford <g@thirdman.co.nz>
2016-09-22 13:28:12 +1200

    fix (DevHome): fixing up the links

--- src/shared/pages/DevHome/DevHome.jsx
+++ src/shared/pages/DevHome/DevHome.jsx
@@ -35,7 +35,7 @@ export default class DevHome extends Component {
 					<Section title="Links" type="section">
 						<div>NOTE: most of these links dont work yet!</div>
 						<Row>
-							<Column occupy="4" of="12">
+							<Column occupy={4} of={12}>
 								<h4>Documentation</h4>
 								<span onClick={this.onClick('/dev/docs')} className={styles.link}>
 									Component Docs
@@ -44,25 +44,25 @@ export default class DevHome extends Component {
 									Api (whiteboard)
 								</span>
 								<span onClick={this.onClick('/dev/icons')} className={styles.link}>Icons</span>
-								<span onClick={this.onClick('https://github.com/NomosAdmin/')} className={styles.link}>Github</span>
+								<a href="https://github.com/NomosAdmin/" className={styles.link}>Github</a>
 							</Column>
-							<Column occupy="4" of="12">
+							<Column occupy={4} of={12}>
 								<h4>Servers</h4>
-								<span onClick={this.onClick('https://dev-api.nomosone.com')} className={styles.link}>dev-api</span>
-								<span onClick={this.onClick('https://test-api.nomosone.com')} className={styles.link}>test-api</span>
-								<span onClick={this.onClick('https://api.nomosone.com')} className={styles.link}>prod: api</span>
-								<span onClick={this.onClick('https://test-app.nomosone.com')} className={styles.link}>test-app</span>
-								<span onClick={this.onClick('https://app.nomosone.com')} className={styles.link}>prod: app</span>
+								<a href="https://dev-api.nomosone.com" className={styles.link}>dev-api</a>
+								<a href="https://test-api.nomosone.com" className={styles.link}>test-api</a>
+								<a href="https://api.nomosone.com" className={styles.link}>prod: api</a>
+								<a href="https://test-app.nomosone.com" className={styles.link}>test-app</a>
+								<a href="https://app.nomosone.com" className={styles.link}>prod: app</a>
 							</Column>
-							<Column occupy="4" of="12">
+							<Column occupy={4} of={12}>
 								<h4>Integrations</h4>
-								<span onClick={this.onClick('https://xero.com')} className={styles.link}>Xero</span>
+								<a href="https://xero.com" className={styles.link}>Xero</a>
 							</Column>
 						</Row>
 					</Section>
 					<Section title="Status" hasDivider>
 						<Row>
-							<Column occupy="12">
+							<Column occupy={12}>
 								<h3>Status</h3>
 							</Column>
 							<div>status will be here</div>
