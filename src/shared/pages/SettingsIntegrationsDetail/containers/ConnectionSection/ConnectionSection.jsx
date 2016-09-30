0263879e5f2972febe18d4719b68de02d977dd52
Khanh Duc Ha <ken.haduc@gmail.com>
2016-09-30 17:00:57 +1300

    extended to handle multiple steps of oauth

--- src/shared/pages/SettingsIntegrationsDetail/containers/ConnectionSection/ConnectionSection.jsx
+++ src/shared/pages/SettingsIntegrationsDetail/containers/ConnectionSection/ConnectionSection.jsx
@@ -8,28 +8,106 @@ import { connect } from '../../../../../utils/state';
 @connect('store')
 export default class ConnectionSection extends Component {
 
+	state = {
+		connecting: false
+	}
+
+	componentDidMount() {
+		const { doneAuth } = this.props;
+		const { app } = this.context.store;
+		const { currentOrg = null } = app;
+		// https://gist.github.com/jlong/2428561
+		let oauthToken = this.getQueryVar('oauth_token');
+		let oauthVerifier = this.getQueryVar('oauth_verifier');
+		let org = this.getQueryVar('org');
+
+		if (doneAuth && oauthToken && oauthVerifier) {
+			client.post(`/organisations/${currentOrg.id}/xero/doneAuth`, {
+				data: {
+					oauth_token: oauthToken,
+					oauth_verifier: oauthVerifier,
+					org
+				}
+			})
+			.then(() => {
+				app.fetchOrgToken();
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+		}
+	}
+
+	getQueryVar(variable) {
+		let query = window.location.search.substring(1);
+		let vars = query.split('&');
+		for (let i = 0; i < vars.length; i++) {
+			let pair = vars[i].split('=');
+			if (decodeURIComponent(pair[0]) === variable) {
+				return decodeURIComponent(pair[1]);
+			}
+		}
+		return false;
+	}
+
+	@autobind
+	connecting(connecting) {
+		this.setState({ connecting });
+	}
+
 	@autobind
 	onClick() {
 		const { currentOrg = null } = this.context.store.app;
-
-		client.post(`/organisations/${currentOrg}/xero/generateToken`)
+		this.connecting(true);
+		client.post(`/organisations/${currentOrg.id}/xero/generateToken`)
 		.then((res) => {
-			console.log(res);
+			let link = res && res.data && res.data[0].url;
+			window.location = link;
 		})
 		.catch((err) => {
 			console.log(err);
+			this.connecting(false);
 		});
 	}
 
 	render() {
 		return (
 			<div>
-				<h3>{'Connection Section'}</h3>
-				<div>{'Here goes the connection section'}</div>
-				<Button
-					content={'Connect to Xero'}
-					onClickProps={this.onClick} />
+				{ this.getAuthSection() }
 			</div>
 		);
+	}
+
+	getAuthSection() {
+		const { doneAuth, token } = this.props;
+		const { connecting } = this.state;
+
+		if (token && token.xero && token.xero.connectedAt) {
+			return (
+				<div>
+					<h3>{'Connection Section'}</h3>
+					<div>{'This organisation is connected to Xero'}</div>
+				</div>
+			);
+		}
+
+		if (!doneAuth) {
+			return (
+				<div>
+					<h3>{'Connection Section'}</h3>
+					<div>{'Here goes the connection section'}</div>
+					<Button
+						content={connecting ? 'Loading ... ' : 'Connect to Xero'}
+						onClickProps={this.onClick} />
+				</div>
+			);
+		} else {
+			return (
+				<div>
+					<h3>{'Connection Section'}</h3>
+					<div>{'Verifying details ....'}</div>
+				</div>
+			);
+		}
 	}
 }
