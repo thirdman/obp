2843e6856cf7b07901f073b1922d61ac8ee2a1ef
Gareth Bedford <g@thirdman.co.nz>
2016-10-04 14:18:05 +1300

    refactor: ObjectInfo
    adding the ability to have additional content in the object info

--- src/shared/pages/TempApiManager/TempApiManager.jsx
+++ src/shared/pages/TempApiManager/TempApiManager.jsx
@@ -1,19 +1,27 @@
 import { Component } from 'react';
 import { Summary } from 'layouts';
-// import { ObjectInfo } from 'components';
-import { Header, Secondary} from 'containers';
+import { Header } from 'containers';
 import { connect } from '../../../utils/state';
 
 @connect('store')
 export default class TempApiManager extends Component {
+
 	render() {
 		return (
 			<Summary>
 				<Header key={'layoutHeader'} title="Nomos one API manager" />
 				<div key={'layoutMain'}>
-					<p>Select your organisation in the sidebar to continue</p>
+					<p>Enable app integrations by selecting an organisation in
+					the sidebar, then the integration you wish to add or edit.</p>
+					<h3>What is an integration?</h3>
+					<p>Integrations allow you to extend the
+							functionality of nomos one with related software you use. For example, if you
+							use Xero accounting software, you can view invoice information right with your
+							entities inside nomos one. </p>
 				</div>
-				<Secondary key={'layoutSecondary'} content=" " />
+				<div key={'layoutSecondary'} >
+					{' '}
+				</div>
 			</Summary>
 		);
 	}
