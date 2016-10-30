e954100aa133a0b604a872947cc688e6575be4b1
Gareth Bedford <g@thirdman.co.nz>
2016-10-31 10:10:21 +1300

    refactor (Row, Column): refactoring to handle classes.

--- src/shared/components/Column/Column.jsx
+++ src/shared/components/Column/Column.jsx
@@ -1,29 +1,52 @@
-import React from 'react';
+import React, {Component, PropTypes} from 'react';
 import cx from 'classnames';
+import classSet from 'react-classset';
 
 const styles = require('./Column.scss');
 
-const Column = ({ isFlex = true, occupy = 12, of = 12, devMode = false, children}) => (
-	<div
-		data-occupy={occupy}
-		data-of={of}
-		className={cx(
-			styles.Column,
-			(devMode ? styles.devMode : ''),
-			(isFlex ? styles.isFlex : ''))} >
-		{children}
-	</div>
-);
+export default class Column extends Component {
+	render() {
+		const {
+			occupy = 12,
+			of = 12,
+			devMode = false,
+			isFlex = true,
+			classNameProps = [],
+			children
+		} = this.props;
+		let classes;
+		let toggleClasses;
 
-Column.propTypes = {
-	occupy: React.PropTypes.number,
-	of: React.PropTypes.number,
-	isFlex: React.PropTypes.bool,
-	devMode: React.PropTypes.bool,
-	children: React.PropTypes.oneOfType([
-		React.PropTypes.arrayOf(React.PropTypes.node),
-		React.PropTypes.node
-	])
-};
+		toggleClasses = classSet({
+			[styles.isFlex]: isFlex,
+			[styles.devMode]: devMode
+		});
 
-export default Column;
+		classes = classNameProps
+			.filter((cName) => { return !!cName; })
+			.map((classV) => styles[classV]).join(' ');
+
+		return (
+			<div
+				data-occupy={occupy}
+				data-of={of}
+				className={cx(
+					styles.Column,
+					classes,
+					toggleClasses)} >
+				{children}
+			</div>
+		);
+	}
+
+	static propTypes = {
+		occupy: PropTypes.number,
+		of: PropTypes.number,
+		isFlex: PropTypes.bool,
+		devMode: PropTypes.bool,
+		children: PropTypes.oneOfType([
+			PropTypes.arrayOf(PropTypes.node),
+			PropTypes.node
+		])
+	};
+}
--- src/shared/components/Row/Row.jsx
+++ src/shared/components/Row/Row.jsx
@@ -1,29 +1,52 @@
-import React from 'react';
+import React, {Component, PropTypes} from 'react';
 import cx from 'classnames';
+import classSet from 'react-classset';
 
 const styles = require('./Row.scss');
 
-const Row = ({ isFlex = true, occupy = 1, of = 1, devMode = false, children}) => (
-	<div
-		data-occupy={occupy}
-		data-of={of}
-		className={cx(
-			styles.Row,
-			(devMode ? styles.devMode : ''),
-			(isFlex ? styles.isFlex : ''))} >
-		{children}
-	</div>
-);
+export default class Row extends Component {
+	render() {
+		const {
+			occupy = 1,
+			of = 1,
+			devMode = false,
+			isFlex = true,
+			classNameProps = [],
+			children
+		} = this.props;
+		let classes;
+		let toggleClasses;
 
-Row.propTypes = {
-	occupy: React.PropTypes.number,
-	of: React.PropTypes.number,
-	isFlex: React.PropTypes.bool,
-	devMode: React.PropTypes.bool,
-	children: React.PropTypes.oneOfType([
-		React.PropTypes.arrayOf(React.PropTypes.node),
-		React.PropTypes.node
-	])
-};
+		toggleClasses = classSet({
+			[styles.isFlex]: isFlex,
+			[styles.devMode]: devMode
+		});
 
-export default Row;
+		classes = classNameProps
+			.filter((cName) => { return !!cName; })
+			.map((classV) => styles[classV]).join(' ');
+
+		return (
+			<div
+				data-occupy={occupy}
+				data-of={of}
+				className={cx(
+					styles.Row,
+					classes,
+					toggleClasses)} >
+				{children}
+			</div>
+		);
+	}
+
+	static propTypes = {
+		occupy: PropTypes.number,
+		of: PropTypes.number,
+		isFlex: PropTypes.bool,
+		devMode: PropTypes.bool,
+		children: PropTypes.oneOfType([
+			PropTypes.arrayOf(PropTypes.node),
+			PropTypes.node
+		])
+	};
+}
